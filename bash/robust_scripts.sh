d92c403a7746f18c1a50cf89092059e93a87b0f6
Malegkin <malegkin@gmail.com>
2018-09-11 02:28:43 +0300

    bash: add: robust

--- bash/robust_scripts.sh
+++ bash/robust_scripts.sh
@@ -4,16 +4,39 @@
 
 echo $?     # show previous exit code
 
+###########################################################################################################################
+#exit the script if any statement returns a non-true return value
+set -e
+#set -o errexit
+
+
+#alternatives:
+lsd 2>&1 >/dev/null
+if [[ "$?" -ne 0 ]]; 
+then 
+    echo "command failed"; 
+#    exit 1; 
+fi
+
+
+lsd 2>&1 >/dev/null || { 
+    echo "command failed"; 
+#    exit 1; 
+}
+
+
+###############
+## || true - ignore error in subprocess
+( echo "test"; exit -1; ) || true       # () - run in new suprocess
+#{ echo "test"; exit -1; } || true       # {} - run in thread
+
+
+###########################################################################################################################
 #exit: if script try to use an uninitialised variable
 set -u        
 set -o nounset
 
-
-
-
-set -e
-set -o errexit
-
+#echo $TEST_AAAAAAAAAAAA
 
 ###########################################################################################################################
 #On a slightly related note, by default bash takes the error status of the last item in a pipeline, which may not be what you want. 
